377406e15779765a6c86f9ac43220ddbd2b88e6d
joseBarry109 <brandon0303weddle@gmail.com>
2022-12-21 09:35:04 +0100

    validate css update

--- components/Authentication/partner/partnerLogin.js
+++ components/Authentication/partner/partnerLogin.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import Link from 'next/link';
 import logo from "@/public/images/logo.png";
 import Image from "next/image";
-import styles from "../validate.css";
+import styles from "../validate.module.css";
 import { loginUser } from '@/redux/User/actions';
 import { useLoginFormValidator } from "./hooks/partner-Login-validator";
 import toast from "@/components/Toast";
