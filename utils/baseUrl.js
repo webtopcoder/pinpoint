3085d610bda5558611f5edae0b323866c893b6f1
webtopcoder <rkvirtys109@gmail.com>
2023-02-23 16:07:57 +0100

    fixed: base url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_API_BASE_URL
+    ? process.env.NEXT_PUBLIC_BASE_URL
     : "http://localhost:3000";
 
 export default baseUrl;
