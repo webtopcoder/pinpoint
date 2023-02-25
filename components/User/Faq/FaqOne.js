3fca500e05dceafac475e5786514e3729fbcce37
h3cksamrat (mint) <53104092+h3cksamrat@users.noreply.github.com>
2023-02-25 08:24:53 +0545

    fixed: faq with loader

--- components/User/Faq/FaqOne.js
+++ components/User/Faq/FaqOne.js
@@ -3,12 +3,20 @@ import { getFaqs } from "@/src/redux/User/actions";
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
+import { LoadingOutlined } from "@ant-design/icons";
+
 import Accordion from "./Accordion";
+import { Spin } from "antd";
+
+const antIcon = <LoadingOutlined style={{ fontSize: 44 }} spin />;
 
 const FaqOne = ({ fetchFaqs, faqs }) => {
+  const [loading, setLoading] = React.useState(false);
   const { notify } = useNotify();
   useEffect(() => {
+    setLoading(true);
     fetchFaqs((_, err) => {
+      setLoading(false);
       if (err) {
         notify("error", err?.response?.data?.message ?? "Something went wrong");
       }
@@ -21,6 +29,18 @@ const FaqOne = ({ fetchFaqs, faqs }) => {
           <div className="box">
             <h3>Getting Started</h3>
             <div className="accordion" id="faqAccordion">
+              {loading && (
+                <Spin
+                  indicator={antIcon}
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                />
+              )}
               <Accordion questionsAnswers={faqs} />
             </div>
           </div>
