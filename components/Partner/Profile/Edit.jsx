0ecb22ad05f01336716932ed075c18fc1d3d5924
webtopcoder <rkvirtys109@gmail.com>
2023-05-15 11:43:34 +0200

    fixed: avatar height issue

--- components/Partner/Profile/Edit.jsx
+++ components/Partner/Profile/Edit.jsx
@@ -243,6 +243,7 @@ const Edit = ({
                                       alt="avatar"
                                       style={{
                                         width: "100%",
+                                        height: "100%"
                                       }}
                                       height={100}
                                       width={100}
