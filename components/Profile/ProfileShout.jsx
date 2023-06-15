63c3ffebd701d9805ba3707725ba5585b8ad61b9
webtopcoder <rkvirtys109@gmail.com>
2023-06-15 09:05:40 +0200

    fixed: testing website live

--- components/Profile/ProfileShout.jsx
+++ components/Profile/ProfileShout.jsx
@@ -81,7 +81,7 @@ const CommentBody = ({ item, likePost, user_id, path }) => {
           icon={<LikeOutlined />}
           key="list-vertical-like-o"
         />
-        {/* <Space style={{
+        <Space style={{
           marginRight: 20,
           marginTop: 20
         }}>
@@ -110,7 +110,7 @@ const CommentBody = ({ item, likePost, user_id, path }) => {
             {expandComments ? <DownOutlined /> : <UpOutlined />}
             View Comments
           </Button>
-        </Space> */}
+        </Space>
       </div>
       <Comments currentUserId={user_id} expand={expand} ownerId={item?.from} setExpandComments={setExpandComments} expandComments={expandComments} setCommentCount={setCommentCount} type="shoutout" id={item?._id} />
     </>
