a2cf9cc51347d28ac047c106756588fafa441dab
webtopcoder <rkvirtys109@gmail.com>
2023-07-29 00:11:05 +0200

    fixed: live website again

--- components/Layout/Partner/Sidebar.jsx
+++ components/Layout/Partner/Sidebar.jsx
@@ -128,7 +128,7 @@ function LeftSidebar({
     ),
     getItem("Settings", `/partner/settings/`, <SettingFilled />),
     getItem("Locations", "/partner/locations/", <EnvironmentFilled />),
-    getItem("Event Schedule", "/partner/event-schedule/", <CalendarOutlined />),
+    // getItem("Event Schedule", "/partner/event-schedule/", <CalendarOutlined />),
     getItem("View Profile", 'sub1', <ProfileFilled />, [
       getItem('Activity', `/profile/${user_id}/activity`),
       getItem('Shout out', `/profile/${user_id}/shout-outs`),
