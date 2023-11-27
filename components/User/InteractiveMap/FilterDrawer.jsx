9cc515fbd9ecca794e5b5d665ddf05cc67405828
unknown <rkvirtys109@gmail.com>
2023-11-26 16:51:44 -0800

    fixed: google map address

--- components/User/InteractiveMap/FilterDrawer.jsx
+++ components/User/InteractiveMap/FilterDrawer.jsx
@@ -1,20 +1,6 @@
-import React, { useEffect, useState, useRef } from "react";
-import { Row, Button, Space, Col, InputNumber, Slider, Form, Select, Tooltip, Drawer, Divider } from "antd";
-import { LoadingOutlined, CloseOutlined, AimOutlined } from '@ant-design/icons';
-import { apiBaseUrl } from "@/utils/baseUrl";
-import useMedia from "@/hooks/useMedia";
-import { categoryService } from "@/services/index";
-import classnames from "classnames";
-
-const antIcon = (
-  <LoadingOutlined
-    style={{
-      fontSize: 24,
-    }}
-    spin
-  />
-);
-const { Option } = Select;
+import React from "react";
+import { Button, Space, Drawer } from "antd";
+import ToolBanner from "@/components/User/InteractiveMap/ToolBanner";
 
 function FilterDrawer({
   open,
@@ -26,67 +12,6 @@ function FilterDrawer({
   inputValue,
   onFinish
 }) {
-
-  const isWebDevice = useMedia('(min-width:700px)');
-  const faviconUrl = `${apiBaseUrl}`;
-  const autoCompleteRef = useRef();
-  const inputRef = useRef();
-  const options = {
-    componentRestrictions: { country: "us" },
-    fields: [
-      "address_components",
-      "adr_address",
-      "formatted_address",
-      "geometry",
-      "name",
-    ],
-  };
-
-  const [categoryInfo, setCategoryInfo] = useState([]);
-  const [subcategoryList, setSubcategoryList] = useState([]);
-  const formatter = (value) => `${value}mile`;
-
-  async function onUpdateField(value) {
-    const result = await categoryService.getSubcategory(value);
-    const subarr = [];
-    result?.subCategories?.map((item, index) => {
-      const subitem = {
-        value: item._id,
-        label: item.name,
-      };
-      subarr.push(subitem);
-    });
-    setSubcategoryList(subarr);
-  }
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      const { latitude, longitude } = position.coords;
-      setPosition({
-        lat: latitude,
-        lng: longitude,
-      });
-    });
-
-    autoCompleteRef.current = new window.google.maps.places.Autocomplete(
-      inputRef.current,
-      options
-    );
-
-    autoCompleteRef.current.addListener("place_changed", async function () {
-      const place = await autoCompleteRef.current.getPlace();
-      setPosition({
-        lat: place.geometry?.location?.lat(),
-        lng: place.geometry?.location?.lng(),
-      });
-    });
-
-    categoryService.getCategory().then(async res => {
-      await setCategoryInfo(res?.allcategories)
-    }).catch((error) => {
-      console.log(error)
-    });
-  }, []);
 
   return (
     <Drawer
@@ -104,118 +29,14 @@ function FilterDrawer({
         </Space>
       }
     >
-      <div className="container">
-        <div className="d-flex flex-wrap mt-4">
-          <div className="me-2">
-            <h3 className="mb-3">Where are the goods at?</h3>
-            <span className="sub-title text-danger">BROUGHT TO YOU BY PINPOINT</span>
-          </div>
-          <p className="ms-auto">
-            <CloseOutlined onClick={onClose} style={{ fontSize: 35, cursor: 'pointer' }} />
-          </p>
-        </div>
-        <Divider />
-        <div className="page-title-content" style={{ textAlign: 'left' }}>
-          <form action="javascript:void(0);">
-            <label>
-              <i className="bx bx-search"></i>
-            </label>
-            <input
-              name="search"
-              type="text"
-              ref={inputRef}
-              className="input-search search-field"
-              placeholder="Enter Address or Share Location"
-            />
-            <Tooltip placement="top" title={"My Location"} className={classnames({ 'd-none': !isWebDevice })}>
-              <Button style={{ top: 6.5 }} size="large" icon={<AimOutlined />} onClick={getCurrentLocation}>
-              </Button>
-            </Tooltip>
-          </form>
-          <Row>
-            <h6 className="mt-4">Search Radius:</h6>
-            <Col xs={24} sm={24} md={19} lg={19} xl={19}>
-              <Slider
-                tooltip={{
-                  formatter,
-                }}
-                trackStyle={{
-                  background: "#175594",
-                }}
-                handleStyle={{
-                  background: "white",
-                }}
-                min={1}
-                max={50}
-                onChange={onChange}
-                value={typeof inputValue === "number" ? inputValue : 0}
-              />
-            </Col>
-            <Col xs={0} sm={0} md={5} lg={5} xl={5}>
-              <InputNumber
-                min={1}
-                max={50}
-                style={{
-                  width: 60,
-                  margin: "0 16px",
-                }}
-                value={inputValue}
-                onChange={onChange}
-              />
-            </Col>
-            <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-              <Form
-                name="validate_other"
-                onFinish={onFinish}
-                // style={{
-                //   maxWidth: 600,
-                // }}
-                layout="vertical"
-              >
-                <Form.Item
-                  name="category"
-                  hasFeedback
-                >
-                  <Select
-                    size="middle"
-                    onChange={(e) => onUpdateField(e)}
-                    placeholder="Select Category">
-                    <Option key={0} value="all">All</Option>
-                    {categoryInfo?.map((option, index) => (
-                      <Option key={index + 1} value={option._id}>{option.name}</Option>
-                    ))}
-                  </Select>
-                </Form.Item>
-                <Form.Item
-                  name="subcategory"
-                  hasFeedback>
-                  <Select
-                    mode="multiple"
-                    showSearch={false}
-                    allowClear
-                    maxTagCount={2}
-                    style={{
-                      width: "100%",
-                    }}
-                    size="middle"
-                    placeholder="Select Subcategory"
-                    options={subcategoryList}
-                  />
-                </Form.Item>
-                <Form.Item label="">
-                  <Button
-                    size="large" style={{
-                      marginTop: 10,
-                      width: "100%",
-                    }} type="primary" htmlType="submit">
-                    Submit
-                  </Button>
-                </Form.Item>
-              </Form>
-            </Col>
-          </Row>
-        </div>
-      </div>
+      <ToolBanner
+        setPosition={setPosition}
+        getCurrentLocation={getCurrentLocation}
+        onChange={onChange}
+        inputValue={inputValue}
+        onFinish={onFinish}
+        onClose={onClose}
+      />
     </Drawer>
   );
 }
--- components/User/InteractiveMap/ToolBanner.jsx
+++ components/User/InteractiveMap/ToolBanner.jsx
@@ -1,9 +1,9 @@
-import { Row, Button, Space, Col, InputNumber, Slider, Form, Select, Tooltip, Input } from "antd";
-import { UnorderedListOutlined, FullscreenOutlined } from '@ant-design/icons';
+import { Row, Button, Space, Col, InputNumber, Slider, Form, Select, Tooltip, Divider } from "antd";
+import { CloseOutlined, AimOutlined } from '@ant-design/icons';
 import React, { useEffect, useState, useRef } from "react";
-import food from "@/public/images/landing/food.png";
-import Image from "next/image";
 import { categoryService } from "@/services/index";
+import classnames from "classnames";
+import useMedia from "@/hooks/useMedia";
 
 const { Option } = Select;
 
@@ -13,12 +13,12 @@ function ToolBanner({
   onChange,
   inputValue,
   onFinish,
-  fullScreen,
-  setAddModalOpen
+  onClose
 }) {
 
   const autoCompleteRef = useRef();
   const inputRef = useRef();
+  const isWebDevice = useMedia('(min-width:700px)');
   const options = {
     componentRestrictions: { country: "us" },
     fields: [
@@ -77,68 +77,73 @@ function ToolBanner({
   }, []);
 
   return (
-    <div className="shout-area followers green-color">
-      <div className="shout-body">
-        <div className="shout-author vcard">
-          <div className="avatar desktop">
-            <Image src={food} alt="user" className="shout-radius" />
-          </div>
-          <form className="search-form" action="javascript:void(0);">
-            <input
-              type="search"
-              className="search-field"
-              ref={inputRef}
-              placeholder="Enter Address or Share Location"
-            />
-            <button onClick={getCurrentLocation}>
-              <i className="bx bx-current-location"></i>
-            </button>
-          </form>
+    <div className="container">
+      <div className="d-flex flex-wrap mt-4">
+        <div className="me-2">
+          <h3 className="mb-3">Where are the goods at?</h3>
+          <span className="sub-title text-danger">BROUGHT TO YOU BY PINPOINT</span>
+
         </div>
-        <div className="shout-metadata">
-          <p>Search Radius:</p>
-          <Row>
-            <Col xs={24} sm={24} md={19} lg={19} xl={19}>
-              <Slider
-                tooltip={{
-                  formatter,
-                }}
-                trackStyle={{
-                  background: "#175594",
-                }}
-                handleStyle={{
-                  background: "white",
-                }}
-                min={1}
-                max={50}
-                onChange={onChange}
-                value={typeof inputValue === "number" ? inputValue : 0}
-              />
-            </Col>
-            <Col xs={0} sm={0} md={3} lg={3} xl={3}>
-              <InputNumber
-                min={1}
-                max={50}
-                style={{
-                  width: 60,
-                  margin: "0 16px",
-                }}
-                value={inputValue}
-                onChange={onChange}
-              />
-            </Col>
-          </Row>
-        </div>
+        <p className="ms-auto">
+          <CloseOutlined onClick={onClose} style={{ fontSize: 35, cursor: 'pointer' }} />
+        </p>
       </div>
-      <div className="shout-button-group">
-        <div className="container">
-          <div className="col-lg-12 col-md-12">
+      <Divider />
+      <div className="page-title-content" style={{ textAlign: 'left' }}>
+        <form action="javascript:void(0);">
+          <label>
+            <i className="bx bx-search"></i>
+          </label>
+          <input
+            name="search"
+            type="text"
+            ref={inputRef}
+            className="input-search search-field"
+            placeholder="Enter Address or Share Location"
+          />
+          <Tooltip placement="top" title={"My Location"} className={classnames({ 'd-none': !isWebDevice })}>
+            <Button style={{ top: 6.5 }} size="large" icon={<AimOutlined />} onClick={getCurrentLocation}>
+            </Button>
+          </Tooltip>
+        </form>
+        <Row>
+          <h6 className="mt-4">Search Radius:</h6>
+          <Col xs={24} sm={24} md={19} lg={19} xl={19}>
+            <Slider
+              tooltip={{
+                formatter,
+              }}
+              trackStyle={{
+                background: "#175594",
+              }}
+              handleStyle={{
+                background: "white",
+              }}
+              min={1}
+              max={50}
+              onChange={onChange}
+              value={typeof inputValue === "number" ? inputValue : 0}
+            />
+          </Col>
+          <Col xs={0} sm={0} md={5} lg={5} xl={5}>
+            <InputNumber
+              min={1}
+              max={50}
+              style={{
+                width: 60,
+                margin: "0 16px",
+              }}
+              value={inputValue}
+              onChange={onChange}
+            />
+          </Col>
+          <Col xs={24} sm={24} md={24} lg={24} xl={24}>
             <Form
               name="validate_other"
               onFinish={onFinish}
-              style={{
-                maxWidth: 600,
-              }}
+              // style={{
+              //   maxWidth: 600,
+              // }}
               layout="vertical"
             >
               <Form.Item
@@ -146,7 +151,7 @@ function ToolBanner({
                 hasFeedback
               >
                 <Select
-                  size="large"
+                  size="middle"
                   onChange={(e) => onUpdateField(e)}
                   placeholder="Select Category">
                   <Option key={0} value="all">All</Option>
@@ -166,7 +171,7 @@ function ToolBanner({
                   style={{
                     width: "100%",
                   }}
-                  size="large"
+                  size="middle"
                   placeholder="Select Subcategory"
                   options={subcategoryList}
                 />
@@ -177,52 +182,12 @@ function ToolBanner({
                     marginTop: 10,
                     width: "100%",
                   }} type="primary" htmlType="submit">
-                  Pinpoint
+                  Submit
                 </Button>
               </Form.Item>
             </Form>
-          </div>
-        </div>
-      </div>
-      <div className="shout-end-group">
-        <div className="container">
-          <Space direction="horizontal" wrap>
-            <Tooltip title="Full Screen">
-              <Button
-                type="primary"
-                style={{
-                  width: 70,
-                  height: 70,
-                }}
-                icon={
-                  <FullscreenOutlined
-                    style={{
-                      fontSize: 40,
-                    }}
-                  />
-                }
-                onClick={() => fullScreen()}
-              />
-            </Tooltip>
-            <Tooltip title="List View">
-              <Button
-                type="primary"
-                style={{
-                  width: 70,
-                  height: 70,
-                }}
-                onClick={() => setAddModalOpen(true)}
-                icon={
-                  <UnorderedListOutlined
-                    style={{
-                      fontSize: 40,
-                    }}
-                  />
-                }
-              />
-            </Tooltip>
-          </Space>
-        </div>
+          </Col>
+        </Row>
       </div>
     </div>
   );
